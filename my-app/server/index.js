c0a67b22287ba481b9e4347f51ddc4c8af978368
Vadim Ichetovkin <ichetowwwkin@mail.ru>
2026-05-05 05:07:44 +0300

    prepare server port for deployment

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -11,14 +11,19 @@ import faqsRoutes from './routes/faqs.js'
 import { initDb } from './db/init.js'
 
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 initDb()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
